1edd9f77ecaed8c34ebe17b9a4462d4430d20b44
Sully <aristotlepoultney@gmail.com>
2020-07-12 16:01:26 -0400

    Updates

---
+++ precache-manifest.dbadf15abea8a3182819cd52181d8529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ff5b6bbfa598e169cb576a25d48140",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "878bd76760cf87c9e7e7",
+    "url": "/test/static/css/main.0e7db47b.chunk.css"
+  },
+  {
+    "revision": "05e7de165b6fe942eb9d",
+    "url": "/test/static/js/2.d3e99135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.d3e99135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878bd76760cf87c9e7e7",
+    "url": "/test/static/js/main.f853e91d.chunk.js"
+  },
+  {
+    "revision": "82d3528bfbafeb33ea62",
+    "url": "/test/static/js/runtime-main.f597de4f.js"
+  }
+]);
